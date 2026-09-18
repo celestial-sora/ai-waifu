320a0ba8606bf45cd9367c085f6ccfe94d3e1fe9
Sora <suphloeksangko@gmail.com>
2026-09-18 11:31:20 +0700

    fix(desktop): import shared companion component

--- app/desktop/page.tsx
+++ app/desktop/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect } from "react";
-import { CompanionApp } from "../page";
+import { CompanionApp } from "../CompanionApp";
 
 export default function DesktopPetPage() {
   useEffect(() => {
